f14fab94bdde0fef692082f08109ce4f718d6b62
vitoraa <vitor.lima@outlook.com>
2021-11-14 19:28:57 -0300

    error handler

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  res.status(400).send({
+    message: 'Something went wrong'
+  });
+};
