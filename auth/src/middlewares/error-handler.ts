a94420f7a62aa157879cd87000d9ec9097f5073b
vitoraa <vitor.lima@outlook.com>
2021-11-16 19:00:16 -0300

    handle custom error

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,13 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../errors/custom-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { RequestValidationError } from "../errors/request-validation-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof RequestValidationError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
-  }
-
-  if (err instanceof DatabaseConnectionError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
 
