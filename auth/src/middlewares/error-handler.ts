0413b8b62f244472e4e09e5b0c631245f775f945
vitoraa <vitor.lima@outlook.com>
2021-11-16 18:30:32 -0300

    error handler checking type of error

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,9 +1,21 @@
 import { NextFunction, Request, Response } from "express";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map(error => {
+      return { message: error.msg, field: error.param }
+    })
+
+    return res.status(400).send({ errors: formattedErrors })
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: err.reason })
+  }
 
   res.status(400).send({
-    message: 'Something went wrong'
+    messages: err.message
   });
 };
