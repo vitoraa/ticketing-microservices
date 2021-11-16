19e2db5113269ec625702c744f86616cc4e2800e
vitoraa <vitor.lima@outlook.com>
2021-11-16 18:42:04 -0300

    create method to serialize errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -4,15 +4,11 @@ import { RequestValidationError } from "../errors/request-validation-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map(error => {
-      return { message: error.msg, field: error.param }
-    })
-
-    return res.status(400).send({ errors: formattedErrors })
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
 
   if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] })
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
 
   res.status(400).send({
