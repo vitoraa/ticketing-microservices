0762fff44f83426d8675a872c9b64c07f8baf972
vitoraa <vitor.lima@outlook.com>
2021-11-16 19:00:04 -0300

    abstract class custom error

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,10 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
   statusCode = 400
   constructor (public errors: ValidationError[]) {
-    super();
+    super('Error on validate request');
 
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
