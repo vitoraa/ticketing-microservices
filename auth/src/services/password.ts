2cee4159f4a7cc016a48edc1d59121e09c4411d7
vitoraa <vitor.lima@outlook.com>
2021-11-17 15:17:41 -0300

    password method to hash and compare using bcrypt

---
+++ auth/src/services/password.ts
@@ -0,0 +1,11 @@
+import * as bcrypt from 'bcrypt';
+
+export class Password {
+  static async toHash (password: string) {
+    return await bcrypt.hash(password, 8);
+  }
+
+  static async compare (storedPassword: string, suppliedPassword: string) {
+    return await bcrypt.compare(storedPassword, suppliedPassword);
+  }
+}
