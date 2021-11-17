beab08422baa17f537b99ca3a4c7e898dc5938e8
vitoraa <vitor.lima@outlook.com>
2021-11-17 10:52:16 -0300

    build method to create a new user

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose';
 
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<any> {
+  build (attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -9,8 +18,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
-})
+});
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
 
 export { User };
