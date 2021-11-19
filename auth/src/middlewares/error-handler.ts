6b00ab3052050164caab4a42078818e3c93c83e5
vitoraa <vitor.lima@outlook.com>
2021-11-19 15:30:56 -0300

    remove unnecessary imports

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import { CustomError } from "../errors/custom-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
-import { RequestValidationError } from "../errors/request-validation-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof CustomError) {
