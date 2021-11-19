63891db1383f68a9bedc7a2d6ac70c143851159a
vitoraa <vitor.lima@outlook.com>
2021-11-19 16:08:53 -0300

    send current user if there is a valid token

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { environment } from '../environment';
 
 const router = express.Router();
 
 router.get('/api/users/current_user', (req, res) => {
-  res.send({
-    currentUser: 'teste',
-  });
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payLoad = jwt.verify(req.session.jwt, environment.jwt_key);
+    return res.send({ currentUser: payLoad });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
+
 });
 
 export { router as currentUserRouter };
