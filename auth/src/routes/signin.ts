cbae98203aa9c713bd8d79a125797b08b6387c97
vitoraa <vitor.lima@outlook.com>
2021-11-19 15:31:24 -0300

    use validaRequest middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
 
 const router = express.Router();
@@ -13,12 +13,7 @@ router.post('/api/users/signin', [
     .trim()
     .notEmpty()
     .withMessage('You must supply a password')
-], async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-  }
+], validateRequest, async (req: Request, res: Response) => {
 
   const { email, password } = req.body;
 
