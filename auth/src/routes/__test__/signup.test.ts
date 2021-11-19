c90193242bd078c538035c3f109dc101d7d78702
vitoraa <vitor.lima@outlook.com>
2021-11-19 17:58:34 -0300

    test: should returns 400 with invalid email

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,13 @@ test('should returns 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+test('should returns 400 with invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'email@email',
+      password: 'password',
+    })
+    .expect(400);
+});
