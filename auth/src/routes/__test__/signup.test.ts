74651e85825b72a88c8d287ecb20b232d7ec3517
vitoraa <vitor.lima@outlook.com>
2021-11-19 17:53:39 -0300

    test: should returns 201 on successful signup

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+test('should returns 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'email@email.com',
+      password: 'password',
+    })
+    .expect(201);
+});
