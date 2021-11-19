53fb281a2a09fc6ad87ebc6f91d294d8b1002885
vitoraa <vitor.lima@outlook.com>
2021-11-19 16:39:58 -0300

    signout remove the session of the user

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,9 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send({
-    currentUser: 'teste',
-  });
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
