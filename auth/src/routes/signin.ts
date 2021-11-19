26f53c0f8546ff1743391b80d750e06ad75af516
vitoraa <vitor.lima@outlook.com>
2021-11-19 15:24:20 -0300

    check params validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,29 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send({
-    currentUser: 'teste',
-  });
+router.post('/api/users/signin', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('You must supply a password')
+], async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email })
+
 });
 
 export { router as signinRouter };
