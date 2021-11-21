44a12eb6fa12b44e055eadcd6a151b2e272428d1
vitoraa <vitor.lima@outlook.com>
2021-11-21 15:53:55 -0300

    create global function to signin

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,11 @@
 import MongoMemoryServer from "mongodb-memory-server-core"
 import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 
@@ -24,3 +30,20 @@ afterAll(() => {
   mongo.stop();
   mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "email@email.com"
+  const password = "password"
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
