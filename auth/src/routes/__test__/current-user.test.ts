46ae6b0cbebe80fef28fcc1ed6470a285bb37f0f
vitoraa <vitor.lima@outlook.com>
2021-11-21 15:54:11 -0300

    use global function signin to get cookie

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,15 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 test('should returns 200 and the current user on successful get current user', async () => {
-  const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'email@email.com',
-      password: 'password',
-    })
-    .expect(201);
-
-  const cookie = authResponse.get('Set-Cookie');
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get('/api/users/current_user')
