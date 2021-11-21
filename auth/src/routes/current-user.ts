0d848729ff4e5063677982fcd1d5513863223324
vitoraa <vitor.lima@outlook.com>
2021-11-21 15:40:49 -0300

    remove unnecessary imports

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,4 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import { environment } from '../environment';
 import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
