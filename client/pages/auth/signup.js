1714fd9bb9cd73d5f6deaa4dbe2815da71b1dea4
vitoraa <vitor.lima@outlook.com>
2021-11-22 18:50:49 -0300

    password and email setState

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,22 @@
 const signUp = () => {
-  return <h1>signUp</h1>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <form>
+      <div className="form-group">
+        <label>Email address</label>
+        <input value={email} onChange={e => setEmail(e.target.value)} className="form-control" />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input value={password} onChange={e => setPassword(e.target.value)} type='password' className="form-control" />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Sign Up
+      </button>
+    </form>
+  );
 };
 
 export default signUp;
