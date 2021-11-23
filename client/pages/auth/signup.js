e63c9cd6e816be76fa8b9059bd5e7bf7ef6361bb
vitoraa <vitor.lima@outlook.com>
2021-11-23 19:01:25 -0300

    show errors on signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const signUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -29,6 +29,12 @@ const signUp = () => {
       <div className="form-group">
         <label>Password</label>
         <input value={password} onChange={e => setPassword(e.target.value)} type='password' className="form-control" />
+      </div>
+      <div className="alert alert-danger">
+        <h4>Oooops....</h4>
+        <ul className="my-0">
+          {errors.map(err => <li key={err.message}>{err.message}</li>)}
+        </ul>
       </div>
       <button type="submit" className="btn btn-primary">
         Sign Up
