10afd9ccb2e6c867614e039eb79c4a5fde7b804f
vitoraa <vitor.lima@outlook.com>
2021-11-24 19:18:49 -0300

    add title

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -23,6 +23,7 @@ const signUp = () => {
 
   return (
     <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email address</label>
         <input value={email} onChange={e => setEmail(e.target.value)} className="form-control" />
