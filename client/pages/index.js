9a8ca835839b5f417daec5ae001c44db7b197606
vitoraa <vitor.lima@outlook.com>
2021-11-24 19:09:45 -0300

    use buildClient

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,24 +1,14 @@
 import axios from "axios";
+import buildClient from "../api/build-client";
 
 const index = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>Teste</h1>;
 };
 
-index.getInitialProps = async ({ req }) => {
-  console.log(req.headers);
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current_user', {
-      headers: req.headers
-    });
-    console.log(data)
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/current_user');
-    return data;
-  }
-
-  return {};
+index.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/current_user');
+  return data;
 }
 
 export default index;
