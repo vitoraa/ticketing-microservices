418e4fb27dee97973224ba2c0d116588baf774c9
vitoraa <vitor.lima@outlook.com>
2021-11-24 18:35:56 -0300

    fix typo

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,14 @@
-const index = () => {
+import axios from "axios";
+
+const index = (data) => {
+  console.log(data);
   return <h1>Teste</h1>;
 };
+
+index.getInitialProps = async () => {
+  const response = await axios.get('/api/users/current_user');
+  console.log(response);
+  return response.data
+}
 
 export default index;
