c154245e66c392455750b3cd157d4d1d8d1ff6cb
vitoraa <vitor.lima@outlook.com>
2021-11-25 18:54:53 -0300

    getinitialprops get current user

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from "../api/build-client";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const App = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header! {currentUser.email}</h1>
+      <Component {...pageProps} />;
+    </div>)
 };
+
+App.getInitialProps = async (appContext) => {
+  const context = appContext.ctx;
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/current_user');
+
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(context);
+  }
+  return { pageProps, ...data };
+}
 
 export default App;
