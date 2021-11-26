29662ce2710bac9cbf305a1065f2a8a9d8759158
vitoraa <vitor.lima@outlook.com>
2021-11-26 14:12:08 -0300

    set link

--- client/components/header.js
+++ client/components/header.js
@@ -7,8 +7,10 @@ const Header = ({ currentUser }) => {
   ]
     .filter(linkConfig => linkConfig)
     .map(({ label, href }) => {
-      return <li key={href}>
-        {label}
+      return <li key={href} className="nav-item">
+        <Link href={href}>
+          <a className="nav-link">{label}</a>
+        </Link>
       </li>
     })
 
