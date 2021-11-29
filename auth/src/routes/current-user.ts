7f7389718a0e1fffe52f424fd1a893a0af768bdc
vitoraa <vitor.lima@outlook.com>
2021-11-29 18:54:47 -0300

    return current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,10 @@
+import express from 'express';
 import { currentUser } from '@vitoraatickets/common';
-import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/current_user', currentUser, (req: Request, res: Response) => {
-  res.send({ currentUser: null });
+router.get('/api/users/current_user', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
-
 
 export { router as currentUserRouter };
