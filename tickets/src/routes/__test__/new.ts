119f07a327f47c319ed4fd51a7e9c85b3b3a8788
vitoraa <vitor.lima@outlook.com>
2021-11-30 18:15:02 -0300

    should return an error if an invalid title is provided

--- tickets/src/routes/__test__/new.ts
+++ tickets/src/routes/__test__/new.ts
@@ -1,10 +1,14 @@
-import express from 'express';
-import { requireAuth } from '@vitoraatickets/common'
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@vitoraatickets/common'
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req, res) => {
-  res.send({});
-});
+router.post('/api/tickets', requireAuth,
+  [body('title')
+    .isString()
+  ], validateRequest, (req: Request, res: Response) => {
+    res.send({});
+  });
 
 export { router as createTicketRouter };
