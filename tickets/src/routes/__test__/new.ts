2a8cd970d13376bf3f135db4a11b10e09659274d
vitoraa <vitor.lima@outlook.com>
2021-11-30 18:20:00 -0300

    should return an error if an invalid price is provided

--- tickets/src/routes/__test__/new.ts
+++ tickets/src/routes/__test__/new.ts
@@ -8,6 +8,11 @@ router.post('/api/tickets', requireAuth,
   [body('title')
     .isString()
     .notEmpty()
+    .withMessage('Title must be provided'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .notEmpty()
+    .withMessage('Price must be provided')
   ], validateRequest, (req: Request, res: Response) => {
     res.send({});
   });
