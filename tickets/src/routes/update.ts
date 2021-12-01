4aeb59a2969ef1b214872a80c22ee21d399ee4af
vitoraa <vitor.lima@outlook.com>
2021-12-01 20:02:22 -0300

    should return 401 if the user is not authenticated

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,10 +1,10 @@
-import { NotFoundError } from '@vitoraatickets/common';
+import { NotFoundError, requireAuth } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', async (req: Request, res: Response) => {
+router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
   const id = req.params.id;
   const ticket = await Ticket.findById(id);
 
