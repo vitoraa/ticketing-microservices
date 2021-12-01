a969a4edbe98843d8c0c95203113c3f0c5395a85
vitoraa <vitor.lima@outlook.com>
2021-12-01 19:56:13 -0300

    should return 404 if the id does not exist

---
+++ tickets/src/routes/update.ts
@@ -0,0 +1,16 @@
+import { NotFoundError } from '@vitoraatickets/common';
+import express, { Request, Response } from 'express';
+import { Ticket } from '../models/ticket';
+
+const router = express.Router();
+
+router.put('/api/tickets/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const ticket = await Ticket.findById(id);
+
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+});
+
+export { router as updateTicketRouter };
