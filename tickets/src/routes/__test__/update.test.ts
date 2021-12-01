be8ab28e5d97e8ae2174ce5227440b82af531c03
vitoraa <vitor.lima@outlook.com>
2021-12-01 20:02:17 -0300

    test: should return 401 if the user is not authenticated

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -6,6 +6,15 @@ test('should return 404 if the id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
   await request(app)
     .put(`/api/tickets/${id}`)
+    .set('Cookie', global.signin())
     .send()
     .expect(404);
+});
+
+test('should return 401 if the user is not authenticated', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send()
+    .expect(401);
 });
