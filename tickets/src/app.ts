16b46cf9e1932ca8eccadaad0d34c3836956b00e
vitoraa <vitor.lima@outlook.com>
2021-12-01 19:30:35 -0300

    add new route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { NotFoundError, errorHandler, currentUser } from '@vitoraatickets/common
 import cookieSession from 'cookie-session';
 import { environment } from './environment';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,6 +16,7 @@ app.use(cookieSession({
 
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError()
