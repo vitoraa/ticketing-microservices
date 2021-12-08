bf31aa33601394930d38439cf06fda123d109ae7
vitoraa <vitor.lima@outlook.com>
2021-12-08 08:52:44 -0300

    should return 400 if the user provides an invalid title or price

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,22 +1,31 @@
-import { NotAuthorizedError, NotFoundError, requireAuth } from '@vitoraatickets/common';
+import { NotAuthorizedError, NotFoundError, requireAuth, validateRequest } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const ticket = await Ticket.findById(id);
+router.put('/api/tickets/:id',
+  [body('title')
+    .isString()
+    .notEmpty()
+    .withMessage('Title must be provided'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .notEmpty()
+    .withMessage('Price must be provided')], validateRequest, requireAuth, async (req: Request, res: Response) => {
+      const id = req.params.id;
+      const ticket = await Ticket.findById(id);
 
-  if (!ticket) {
-    throw new NotFoundError();
-  }
+      if (!ticket) {
+        throw new NotFoundError();
+      }
 
-  if (ticket.userId !== req.currentUser!.id) {
-    throw new NotAuthorizedError();
-  }
+      if (ticket.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+      }
 
-  res.send(ticket);
-});
+      res.send(ticket);
+    });
 
 export { router as updateTicketRouter };
