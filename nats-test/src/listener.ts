4652a39d27b1e00baffe8a3b8b792ec9b38eba94
vitoraa <vitor.lima@outlook.com>
2021-12-08 14:45:55 -0300

    set manual ack

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -3,17 +3,21 @@ import { randomBytes } from 'crypto';
 
 console.clear();
 
+
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
+const options = stan.subscriptionOptions().setManualAckMode(true);
+
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created', 'listenerQueueGroup');
+  const subscription = stan.subscribe('ticket:created', 'listenerQueueGroup', options);
 
   subscription.on('message', (msg: Message) => {
     const data = JSON.parse(msg.getData() as string);
     console.log(`Received a message #${msg.getSequence()}, with data ${data}`);
+    msg.ack();
   });
 });
