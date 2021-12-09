95f0006f25d721b44589437ac3b58af2316f17d6
vitoraa <vitor.lima@outlook.com>
2021-12-09 20:19:31 -0300

    generic publisher

---
+++ nats-test/src/events/base-publisher.ts
@@ -0,0 +1,21 @@
+import { Subjects } from "./subjects";
+import { Stan } from "node-nats-streaming";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Publisher<T extends Event> {
+  abstract subject: T['subject'];
+
+  constructor (private readonly client: Stan) {
+
+  }
+
+  publish (data: T['data']) {
+    this.client.publish(this.subject, data, () => {
+      console.log('Event published');
+    });
+  }
+}
