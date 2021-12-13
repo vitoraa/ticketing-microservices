c900cadeb43235138f3cbc1dbba1d0163cf6a9ce
vitoraa <vitor.lima@outlook.com>
2021-12-13 09:32:02 -0300

    return a Promise

--- nats-test/src/events/base-publisher.ts
+++ nats-test/src/events/base-publisher.ts
@@ -11,9 +11,13 @@ export abstract class Publisher<T extends Event> {
 
   constructor (private readonly client: Stan) { }
 
-  publish (data: T['data']) {
-    this.client.publish(this.subject, JSON.stringify(data), () => {
-      console.log('Event published');
-    });
+  publish (data: T['data']): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), (err) => {
+        if (err) return reject(err);
+
+        resolve();
+      });
+    })
   }
 }
