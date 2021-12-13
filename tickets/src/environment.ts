acc6be52a34aa6184e94fe39a331893d9568c042
vitoraa <vitor.lima@outlook.com>
2021-12-13 15:56:33 -0300

    add env variables

--- tickets/src/environment.ts
+++ tickets/src/environment.ts
@@ -2,4 +2,7 @@ export const environment = {
   jwt_key: process.env.JWT_KEY || 'asdf',
   node_env: process.env.NODE_ENV || 'development',
   mongo_uri: process.env.MONGO_URI || 'mongodb://tickets-mongo-srv:27017/tickets',
+  nats_url: process.env.NATS_URL || 'http://nats-srv:4222',
+  nats_client_id: process.env.NATS_CLIENT_ID || 'asdff',
+  nats_cluster_id: process.env.NATS_CLUSTER_ID || 'ticketing'
 };
