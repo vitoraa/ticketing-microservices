b958a9d2f4d86d1aec12cf8ae2c27b749316034b
vitoraa <vitor.lima@outlook.com>
2021-12-13 09:35:18 -0300

    consoling logs

--- nats-test/src/events/base-publisher.ts
+++ nats-test/src/events/base-publisher.ts
@@ -15,7 +15,7 @@ export abstract class Publisher<T extends Event> {
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (err) => {
         if (err) return reject(err);
-
+        console.log('Event published to subject', this.subject);
         resolve();
       });
     })
