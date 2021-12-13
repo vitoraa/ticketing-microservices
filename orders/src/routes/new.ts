4b5317e664bf0248c5954c8af58bda0bfec0a4de
vitoraa <vitor.lima@outlook.com>
2021-12-13 17:08:29 -0300

    add validation

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,15 @@
+import { requireAuth, validateRequest } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+  body('ticketId')
+    .not()
+    .isEmpty()
+    .withMessage('TicketId must be provided')
+], validateRequest, async (req: Request, res: Response) => {
   res.send({});
 });
 
