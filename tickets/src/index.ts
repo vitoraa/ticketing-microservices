61108325c5431237f5fea3ff1be872ed84b3b042
vitoraa <vitor.lima@outlook.com>
2021-12-13 13:35:32 -0300

    fix url and add on close event

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,7 +6,14 @@ import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   try {
-    await natsWrapper.connect('ticketing', 'hcsbscv', 'http://nats-srv:422');
+    await natsWrapper.connect('ticketing', 'hcsbscv', 'http://nats-srv:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(environment.mongo_uri);
     console.log('Connected to MongoDB')
   } catch (err) {
