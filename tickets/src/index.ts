ec2fe1ab525173df70af918545f6bbf2c0ce7dbb
vitoraa <vitor.lima@outlook.com>
2021-12-13 11:34:16 -0300

    connect to NATS

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -2,9 +2,11 @@ import 'express-async-errors';
 import mongoose from 'mongoose'
 import { app } from './app';
 import { environment } from './environment';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   try {
+    await natsWrapper.connect('ticketing', 'hcsbscv', 'http://nats-srv:422');
     await mongoose.connect(environment.mongo_uri);
     console.log('Connected to MongoDB')
   } catch (err) {
