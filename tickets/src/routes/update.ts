9711500823eb7a7aa18d9b47d057ad53fc7d052d
vitoraa <vitor.lima@outlook.com>
2021-12-13 13:50:00 -0300

    send message to NATS when update ticket

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,7 +1,9 @@
 import { NotAuthorizedError, NotFoundError, requireAuth, validateRequest } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
 import { Ticket } from '../models/ticket';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -31,7 +33,12 @@ router.put('/api/tickets/:id',
       });
 
       await ticket.save();
-
+      await new TicketUpdatedPublisher(natsWrapper.client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price,
+        userId: ticket.userId
+      });
       res.send(ticket);
     });
 
