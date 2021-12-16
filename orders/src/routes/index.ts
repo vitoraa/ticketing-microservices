ae7f109177b6e61af6fc358522d739e6ac9264c9
vitoraa <vitor.lima@outlook.com>
2021-12-16 12:16:53 -0300

    should return the orders of the user

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { requireAuth } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
-  res.send({});
+  const userId = req.currentUser!.id;
+  const orders = await Order.find({ userId });
+  res.status(200).send(orders);
 });
 
 export { router as indexOrderRouter };
