99981b080677a0a226842dae95763bee283da823
vitoraa <vitor.lima@outlook.com>
2021-12-16 09:59:26 -0300

    check if there is an order for the ticket

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,8 @@
-import { NotFoundError, requireAuth, validateRequest } from '@vitoraatickets/common';
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Order } from '../models/order';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
@@ -18,6 +19,21 @@ router.post('/api/orders', requireAuth, [
 
   if (!ticket) {
     throw new NotFoundError();
+  }
+
+  const existingOrder = await Order.findOne({
+    ticket: ticket,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete
+      ]
+    }
+  });
+
+  if (existingOrder) {
+    throw new BadRequestError();
   }
 
   res.send({});
