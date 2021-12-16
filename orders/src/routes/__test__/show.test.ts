4e5b71d10eb59fdd78c1e5fe4b317098f5ad84bf
vitoraa <vitor.lima@outlook.com>
2021-12-16 15:01:50 -0300

    test: should return the order of the user

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -26,3 +26,61 @@ test('should returns status other than 401 if the user is signed in', async () =
 
   expect(response.status).not.toEqual(401);
 });
+
+test('should return the order of the user', async () => {
+  const user1 = global.signin();
+  const user2 = global.signin();
+
+  const ticket = Ticket.build({
+    price: 10,
+    title: 'Title 1'
+  });
+  await ticket.save();
+
+  const ticket2 = Ticket.build({
+    price: 20,
+    title: 'Title 2'
+  });
+  await ticket2.save();
+
+  const ticket3 = Ticket.build({
+    price: 30,
+    title: 'Title 3'
+  });
+  await ticket3.save();
+
+  const { body: orderOne } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({
+      ticketId: ticket2.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', user2)
+    .send({
+      ticketId: ticket3.id,
+    })
+    .expect(201);
+
+
+  const response = await request(app)
+    .get(`/api/orders/${orderOne.id}`)
+    .set('Cookie', user1)
+    .send({})
+
+  expect(response.status).toEqual(200);
+  expect(response.body.id).toEqual(orderOne.id);
+  expect(response.body.ticket.title).toEqual('Title 1');
+  expect(response.body.ticket.price).toEqual(10);
+});
