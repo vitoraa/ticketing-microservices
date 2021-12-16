4d3a8d8e8ebeb6c42acd210e305c9fbe9a4e0a22
vitoraa <vitor.lima@outlook.com>
2021-12-16 09:53:09 -0300

    add ticketId validation and check if ticket exists

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,8 @@
-import { requireAuth, validateRequest } from '@vitoraatickets/common';
+import { NotFoundError, requireAuth, validateRequest } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import mongoose from 'mongoose';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -9,7 +11,15 @@ router.post('/api/orders', requireAuth, [
     .not()
     .isEmpty()
     .withMessage('TicketId must be provided')
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
 ], validateRequest, async (req: Request, res: Response) => {
+  const { ticketId } = req.body;
+  const ticket = await Ticket.findById(ticketId);
+
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
   res.send({});
 });
 
