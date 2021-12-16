adddb911f9b6e226cfd2d71c281b10af0d192d82
vitoraa <vitor.lima@outlook.com>
2021-12-16 15:28:19 -0300

    should return 404 if the order does not exist or not authorized

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,18 @@
-import { requireAuth } from '@vitoraatickets/common';
+import { NotFoundError, requireAuth } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const orderId = req.params.orderId;
+  const userId = req.currentUser!.id;
+  const order = await Order.findOne({ userId, id: orderId });
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
   res.send({});
 });
 
