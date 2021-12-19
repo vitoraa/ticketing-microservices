e815edbd79fae74e0e85c1605f9a1aade56abc94
vitoraa <vitor.lima@outlook.com>
2021-12-19 15:03:49 -0300

    update ticket on event

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -6,6 +6,19 @@ import { Ticket } from "../../models/ticket";
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent>{
   readonly subject = Subjects.TicketUpdated;
   queueGroupName = environment.queue_group_name;
-  async onMessage (data: TicketUpdatedEvent['data'], msg: Message) { }
+  async onMessage (data: TicketUpdatedEvent['data'], msg: Message) {
+    const { id, title, price } = data;
+    const ticket = await Ticket.findById(id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    ticket.title = title;
+    ticket.price = price;
+    await ticket.save();
+
+    msg.ack();
+  }
 
 }
