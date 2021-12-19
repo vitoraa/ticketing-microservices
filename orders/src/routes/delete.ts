edef3abaca6ef9309263b3491b66c59d5aaa9367
vitoraa <vitor.lima@outlook.com>
2021-12-19 14:18:46 -0300

    should publish an event

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,20 +1,29 @@
 import { NotFoundError, OrderStatus, requireAuth } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
 import { Order } from '../models/order';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
   const orderId = req.params.orderId;
   const userId = req.currentUser!.id;
-  const order = await Order.findOne({ userId, id: orderId });
+  const order = await Order.findOne({ userId, id: orderId }).populate('ticket');
 
   if (!order) {
     throw new NotFoundError();
   }
 
   order.status = OrderStatus.Cancelled;
-  await order.save()
+  await order.save();
+
+  await new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id
+    }
+  });
 
   res.send(order);
 });
