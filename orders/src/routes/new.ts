ff574c68468edab38d12ef245054e29433f3d283
vitoraa <vitor.lima@outlook.com>
2021-12-19 14:07:08 -0300

    should publish an event

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -2,8 +2,10 @@ import { BadRequestError, NotFoundError, requireAuth, validateRequest } from '@v
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
 import { Order, OrderStatus } from '../models/order';
 import { Ticket } from '../models/ticket';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -39,6 +41,17 @@ router.post('/api/orders', requireAuth, [
     ticket
   });
   await order.save();
+
+  await new OrderCreatedPublisher(natsWrapper.client).publish({
+    id: order.id,
+    status: order.status,
+    userId: order.userId,
+    expiresAt: order.expiresAt.toISOString(),
+    ticket: {
+      id: ticket.id,
+      price: ticket.price
+    }
+  });
 
   res.status(201).send(order);
 });
