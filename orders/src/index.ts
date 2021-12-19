b41f2bc897e8d02899a17c33b21751a205274597
vitoraa <vitor.lima@outlook.com>
2021-12-19 16:21:30 -0300

    listen event on start application

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -2,6 +2,8 @@ import 'express-async-errors';
 import mongoose from 'mongoose'
 import { app } from './app';
 import { environment } from './environment';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
@@ -13,6 +15,9 @@ const start = async () => {
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(environment.mongo_uri);
     console.log('Connected to MongoDB')
