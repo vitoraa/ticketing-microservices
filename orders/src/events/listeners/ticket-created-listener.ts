11d8bcca5837d89d514ac0322fe080b024db8880
vitoraa <vitor.lima@outlook.com>
2021-12-19 14:54:51 -0300

    save ticket on message

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,10 +1,20 @@
 import { Listener, Subjects, TicketCreatedEvent } from "@vitoraatickets/common";
 import { Message } from "node-nats-streaming";
 import { environment } from "../../environment";
+import { Ticket } from "../../models/ticket";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
   readonly subject = Subjects.TicketCreated;
   queueGroupName = environment.queue_group_name;
-  onMessage (data: TicketCreatedEvent['data'], msg: Message): void { }
+  async onMessage (data: TicketCreatedEvent['data'], msg: Message) {
+    const { id, title, price } = data;
+
+    const ticket = Ticket.build({
+      id, title, price
+    });
+    await ticket.save();
+
+    msg.ack();
+  }
 
 }
