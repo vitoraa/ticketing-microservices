f3f71ab6a32f397c2d944c39b9d841e8f76f9e2c
vitoraa <vitor.lima@outlook.com>
2021-12-19 14:18:52 -0300

    test: should publish an event

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import { OrderStatus } from '@vitoraatickets/common';
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 test('should have a route handler listening to api/orders for delete request', async () => {
   const response = await request(app)
@@ -90,4 +91,29 @@ test('should change order status of order to Cancelled', async () => {
   expect(response.status).toEqual(200);
   expect(response.body.id).toEqual(orderOne.id);
   expect(response.body.status).toEqual(OrderStatus.Cancelled);
+});
+
+test('should publish an event', async () => {
+  const user1 = global.signin();
+
+  const ticket = Ticket.build({
+    price: 10,
+    title: 'Title 1'
+  });
+  await ticket.save();
+
+  const { body: orderOne } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${orderOne.id}`)
+    .set('Cookie', user1)
+    .send({})
+
+  expect(natsWrapper.client.publish).toHaveBeenCalledTimes(2);
 });
