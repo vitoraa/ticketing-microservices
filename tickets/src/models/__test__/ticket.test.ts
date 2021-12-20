cef2ae453e08b671ef00efe99a9bad5d15102ce6
vitoraa <vitor.lima@outlook.com>
2021-12-20 11:55:18 -0300

    test: should increment the version number on multiple saves

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -18,3 +18,18 @@ test('should implement optimisitc concurrency control', async () => {
   await firstInstance!.save();
   await expect(secondInstance!.save()).rejects.toThrow();
 });
+
+test('should increment the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'Title 1',
+    price: 10,
+    userId: '123'
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
