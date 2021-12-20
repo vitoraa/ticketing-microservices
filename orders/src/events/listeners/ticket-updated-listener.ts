476ce58e566a684e5b40d101c61ea7b9b33d8cd7
vitoraa <vitor.lima@outlook.com>
2021-12-20 14:04:03 -0300

    find by id and version

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -7,8 +7,11 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent>{
   readonly subject = Subjects.TicketUpdated;
   queueGroupName = environment.queue_group_name;
   async onMessage (data: TicketUpdatedEvent['data'], msg: Message) {
-    const { id, title, price } = data;
-    const ticket = await Ticket.findById(id);
+    const { id, version, title, price } = data;
+    const ticket = await Ticket.findOne({
+      _id: id,
+      version: version - 1
+    });
 
     if (!ticket) {
       throw new Error('Ticket not found');
