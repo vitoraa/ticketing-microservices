ea7a052a26be9e6bd81330bbf29c8ff972b93619
vitoraa <vitor.lima@outlook.com>
2021-12-20 14:56:06 -0300

    test: should create and save a ticket

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,35 @@
+import { TicketCreatedListener } from '../ticket-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { TicketCreatedEvent } from '@vitoraatickets/common';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  const listener = new TicketCreatedListener(natsWrapper.client);
+
+  const data: TicketCreatedEvent['data'] = {
+    version: 0,
+    id: new mongoose.Types.ObjectId().toHexString(),
+    price: 10,
+    title: 'title',
+    userId: new mongoose.Types.ObjectId().toHexString()
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  }
+
+  return { listener, data, msg };
+};
+
+test('should create and save a ticket', async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  const ticket = await Ticket.findById(data.id);
+
+  expect(ticket).toBeDefined();
+  expect(ticket!.title).toEqual(data.title);
+});
