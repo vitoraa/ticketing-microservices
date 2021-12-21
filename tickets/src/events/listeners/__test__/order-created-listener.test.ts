e4d8ea32b37038fe979c1c1bb50de5439cc84988
vitoraa <vitor.lima@outlook.com>
2021-12-21 09:11:34 -0300

    test: publishes an event

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -51,3 +51,10 @@ test('should call msg.ack', async () => {
 
   expect(msg.ack).toHaveBeenCalled();
 });
+
+test('publishes an event', async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
