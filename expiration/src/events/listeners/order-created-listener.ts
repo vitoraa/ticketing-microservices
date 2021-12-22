b830d673f398c65691d96b8df3da16c73f28c609
vitoraa <vitor.lima@outlook.com>
2021-12-22 14:26:08 -0300

    improve readability

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -8,11 +8,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
 
   async onMessage (data: OrderCreatedEvent['data'], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
-    await expirationQueue.add({
-      orderId: data.id,
-    }, {
-      delay
-    });
+    await expirationQueue.add({ orderId: data.id }, { delay });
 
     msg.ack();
   }
