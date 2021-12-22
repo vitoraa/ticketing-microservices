3dbd7187837073f4d699fc0c7ee40339511cb4e8
vitoraa <vitor.lima@outlook.com>
2021-12-22 16:34:42 -0300

    test: should create and save an Order

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,39 @@
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCreatedEvent, OrderStatus } from '@vitoraatickets/common';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { OrderCreatedListener } from '../order-created-listener';
+import { Order } from '../../../models/order';
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    expiresAt: new Date().toISOString(),
+    ticket: {
+      id: new mongoose.Types.ObjectId().toHexString(),
+      price: 10,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  }
+
+  return { listener, data, msg };
+};
+
+test('should create and save an Order', async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order).toBeDefined();
+  expect(order!.price).toEqual(data.ticket.price);
+});
