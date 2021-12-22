b325ff6d8fb1853eeeb79c2682634e3a2bd4e049
vitoraa <vitor.lima@outlook.com>
2021-12-22 16:07:00 -0300

    initial commit

---
+++ payments/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import 'express-async-errors';
+import { NotFoundError, errorHandler, currentUser } from '@vitoraatickets/common';
+import cookieSession from 'cookie-session';
+import { environment } from './environment';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(cookieSession({
+  signed: false,
+  secure: environment.node_env !== 'test',
+}));
+
+app.use(currentUser);
+
+app.all('*', async () => {
+  throw new NotFoundError()
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,48 @@
+import MongoMemoryServer from "mongodb-memory-server-core"
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
+import { environment } from "../environment";
+
+declare global {
+  var signin: () => string[];
+}
+
+let mongo: any;
+
+jest.mock('../nats-wrapper');
+jest.setTimeout(15000);
+
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(() => {
+  mongo.stop();
+  mongoose.connection.close();
+});
+
+global.signin = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "email@email.com"
+  };
+
+  const token = jwt.sign(payload, environment.jwt_key);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`express:sess=${base64}`];
+};
