302fea6141c95b1cfb889045e776fa5a4d15b513
vitoraa <vitor.lima@outlook.com>
2021-12-22 12:04:50 -0300

    add redis host

--- expiration/src/environment.ts
+++ expiration/src/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   nats_url: process.env.NATS_URL || 'http://nats-srv:4222',
   nats_client_id: process.env.NATS_CLIENT_ID || 'asdff',
   nats_cluster_id: process.env.NATS_CLUSTER_ID || 'expiration',
+  redis_host: process.env.REDIS_HOST || 'expiration-redis-srv',
   queue_group_name: 'expirations-service'
 };
