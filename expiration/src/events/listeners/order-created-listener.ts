e20b5e25f61b4d64d05482e6e920a8cbf603df4c
vitoraa <vitor.lima@outlook.com>
2021-12-22 14:09:28 -0300

    define the dealy. the expiration date minus the current date

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -7,8 +7,11 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
   queueGroupName = environment.queue_group_name;
 
   async onMessage (data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
     await expirationQueue.add({
       orderId: data.id,
+    }, {
+      delay
     });
 
     msg.ack();
