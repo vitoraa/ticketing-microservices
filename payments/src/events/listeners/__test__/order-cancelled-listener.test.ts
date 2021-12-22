f6f62e3da8c1c566b96e9f27a03daeea31d6bcfc
vitoraa <vitor.lima@outlook.com>
2021-12-22 16:58:31 -0300

    test: should update the Order as cancelled

---
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,44 @@
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCancelledEvent, OrderStatus } from '@vitoraatickets/common';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../../models/order';
+import { OrderCancelledListener } from '../order-cancelled-listener';
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    price: 10,
+    userId: 'user-id',
+    version: 0
+  });
+  await order.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: order.id,
+    version: 0,
+    ticket: {
+      id: new mongoose.Types.ObjectId().toHexString(),
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  }
+
+  return { listener, data, msg };
+};
+
+test('should update the Order as cancelled', async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order).toBeDefined();
+  expect(order!.status).toEqual(OrderStatus.Cancelled);
+});
