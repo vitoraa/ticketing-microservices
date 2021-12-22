1c04aca39f01b85adb4d2e4fa723c0d2e6cc5406
vitoraa <vitor.lima@outlook.com>
2021-12-22 13:59:52 -0300

    add job on the message queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,17 @@
 import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from '@vitoraatickets/common';
 import { Message } from 'node-nats-streaming';
 import { environment } from '../../environment';
+import { expirationQueue } from '../../queue/expiration-queue';
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
   readonly subject = Subjects.OrderCreated;
   queueGroupName = environment.queue_group_name;
 
-  onMessage (data: OrderCreatedEvent['data'], msg: Message): void {
-    throw new Error('Method not implemented.');
+  async onMessage (data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
   }
 
 }
