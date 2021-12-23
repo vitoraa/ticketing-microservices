6d63a7b150b9f65c14c332ae88119471b7d07ef5
vitoraa <vitor.lima@outlook.com>
2021-12-23 11:00:49 -0300

    test: should only be accessed if the user is signed in

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -8,3 +8,11 @@ test('should have a route handler listening to api/payments for post request', a
 
   expect(response.status).not.toEqual(404);
 });
+
+
+test('should only be accessed if the user is signed in', async () => {
+  await request(app)
+    .post('/api/payments')
+    .send({})
+    .expect(401);
+});
