8ec37b30cc2fa7f6c0f298b6fe274ed27e7eaba6
vitoraa <vitor.lima@outlook.com>
2021-12-23 11:07:10 -0300

    should return an error if an invalid payment is provided

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,10 +1,17 @@
-import { requireAuth } from '@vitoraatickets/common';
+import { requireAuth, validateRequest } from '@vitoraatickets/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/payments', requireAuth, async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post('/api/payments', requireAuth,
+  [
+    body('token').not().isEmpty(),
+    body('orderId').not().isEmpty()
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  });
 
 export { router as createChargeRouter };
