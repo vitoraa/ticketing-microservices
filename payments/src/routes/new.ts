4df7c026e401d0e8dc3dd58a00554aa6153bd6ec
vitoraa <vitor.lima@outlook.com>
2021-12-27 11:44:04 -0300

    should create an instance of payment with valid inputs

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -2,6 +2,7 @@ import { BadRequestError, NotAuthorizedError, NotFoundError, OrderStatus, requir
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Order } from '../models/order';
+import { Payment } from '../models/payment';
 import { stripe } from '../stripe';
 
 const router = express.Router();
@@ -28,11 +29,18 @@ router.post('/api/payments', requireAuth,
       throw new BadRequestError('Not possible to pay for a cancelled order');
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'usd',
       amount: order.price * 100,
       source: token
     });
+
+    const payment = await Payment.build({
+      orderId,
+      stripeId: charge.id
+    });
+
+    await payment.save();
 
     res.status(201).send({});
   });
