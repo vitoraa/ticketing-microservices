ffeba7fcdf277f38d2ba2e7ba6c510c732d6be8e
vitoraa <vitor.lima@outlook.com>
2021-12-27 12:29:55 -0300

    update order status on payment

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,23 @@
+import { Listener, Subjects, PaymentCreatedEvent, OrderStatus } from "@vitoraatickets/common";
+import { Message } from "node-nats-streaming";
+import { environment } from "../../environment";
+import { Order } from "../../models/order";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent>{
+  readonly subject = Subjects.PaymentCreated;
+  queueGroupName = environment.queue_group_name;
+  async onMessage (data: PaymentCreatedEvent['data'], msg: Message) {
+    const { orderId, stripeId } = data;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.Complete,
+    });
+    await order.save();
+    msg.ack();
+  }
+}
--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import { app } from './app';
 import { environment } from './environment';
 import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
+import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 import { natsWrapper } from './nats-wrapper';
@@ -20,6 +21,7 @@ const start = async () => {
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
     new ExpirationCompleteListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(environment.mongo_uri);
     console.log('Connected to MongoDB')
