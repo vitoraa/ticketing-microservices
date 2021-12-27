5a9d0dec3fc7dab9980d7198b2db7a7dee4d4acd
vitoraa <vitor.lima@outlook.com>
2021-12-27 14:25:17 -0300

    formatting text

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,10 @@
 import buildClient from "../api/build-client";
 
 const index = ({ currentUser }) => {
-  return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
+  return currentUser ?
+    <h1>You are signed in</h1>
+    :
+    <h1>You are not signed in</h1>
 };
 
 index.getInitialProps = async (context) => {
