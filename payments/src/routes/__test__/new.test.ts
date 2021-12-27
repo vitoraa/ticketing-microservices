f50edec1414e3e5f34f3f804f2cf6d29db19302f
vitoraa <vitor.lima@outlook.com>
2021-12-27 11:44:13 -0300

    test: should create an instance of payment with valid inputs

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { Order } from '../../models/order';
 import { OrderStatus } from '@vitoraatickets/common';
 import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
 
 jest.mock('../../stripe');
 
@@ -134,4 +135,31 @@ test('should returns a 201 with a valid inputs', async () => {
   expect(chargeOptions.source).toEqual('tok_visa');
   expect(chargeOptions.amount).toEqual(10 * 100);
   expect(chargeOptions.currency).toEqual('usd');
+});
+
+test('should create an instance of payment with valid inputs', async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    price: 10,
+    version: 0,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(order.userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+  const chargeResult = await (stripe.charges.create as jest.Mock).mock.results[0].value;
+  const payment = await Payment.find({});
+  expect(payment).not.toBeNull();
+  expect(payment.length).toEqual(1);
+  expect(payment[0].orderId).toEqual(order.id);
+  expect(payment[0].stripeId).toEqual(chargeResult.id);
 });
