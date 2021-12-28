8ff9314ad6b8023f1072bf3d629faa811cc59f20
vitoraa <vitor.lima@outlook.com>
2021-12-28 10:04:08 -0300

    retrieve tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,15 @@
-const index = ({ currentUser }) => {
+const index = ({ currentUser, tickets }) => {
+  console.log(tickets);
   return currentUser ?
     <h1>You are signed in</h1>
     :
     <h1>You are not signed in</h1>
 };
 
-index.getInitialProps = async (context) => {
-  return {};
+index.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
 }
 
 export default index;
