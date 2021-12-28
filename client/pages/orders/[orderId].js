d39a7ceb627e22f441009b102677c0233d08b530
vitoraa <vitor.lima@outlook.com>
2021-12-28 17:01:20 -0300

    add stripe checkout

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from 'react-stripe-checkout';
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
   useEffect(() => {
     const findTimeLeft = () => {
@@ -21,7 +22,13 @@ const OrderShow = ({ order }) => {
   }
 
   return (
-    <div>Time left to pay: {timeLeft} seconds</div>
+    <div>Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey="pk_test_51K9vZzAst0WsM9QjuJXunaoQgacWcvg0L3FZIVXnkioxHJSkuCObRo5tuI3iLKd0m57eL43VK57y0GKwBrM4dkJJ00UzPoxYY3"
+        amount={order.ticket.price * 100}
+        email={currentUser.email} />
+    </div>
   );
 }
 
