e1a532164b437e67e87ce9b8785a48ab85bcd53a
vitoraa <vitor.lima@outlook.com>
2021-12-28 10:14:27 -0300

    show tickets in a table

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,27 @@
 const index = ({ currentUser, tickets }) => {
-  console.log(tickets);
-  return currentUser ?
-    <h1>You are signed in</h1>
-    :
-    <h1>You are not signed in</h1>
+  const ticketList = tickets.map(ticket =>
+  (
+    <tr key={ticket.id}>
+      <td>{ticket.title}</td>
+      <td>{ticket.price}</td>
+    </tr>)
+  );
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ticketList}
+        </tbody>
+      </table>
+    </div>
+  )
 };
 
 index.getInitialProps = async (context, client, currentUser) => {
