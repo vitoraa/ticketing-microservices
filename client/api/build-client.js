b05fbb2f1ad1b1305cf07c2961a621728540fb88
vitoraa <vitor.lima@outlook.com>
2022-01-06 17:29:38 -0300

    change path

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://www.vitorticket.xyz/',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers
     });
   } else {
